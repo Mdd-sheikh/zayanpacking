149b28d0b6716493819ba95aecb280d4a72670c3
Your Name <your-email@example.com>
2026-01-28 03:34:35 -0800

    fix line endings

--- frontend/zayanpack/src/App.jsx
+++ frontend/zayanpack/src/App.jsx
@@ -5,19 +5,22 @@ import Home from './pages/Home/Home'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import Popup from './components/loginpopup/Popup'
+import MobileMenu from './mobile/MobileMenu'
 
 function App() {
   const [showpopup, setShowPopup] = useState(false)
+  const [mobileMenu, setMobileMenu] = useState(false)
 
   return (
     <>
-     {showpopup?<Popup showpopup = {showpopup} setShowPopup={setShowPopup}/>:""}
+      {mobileMenu ? <MobileMenu  setMobileMenu = {setMobileMenu}/> : <></>}
+      {showpopup ? <Popup showpopup={showpopup} setShowPopup={setShowPopup} /> : ""}
       <div className="home">
-        <Navbar showpopup = {showpopup} setShowPopup={setShowPopup}/>
+        <Navbar showpopup={showpopup} setShowPopup={setShowPopup} />
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </>
   )
