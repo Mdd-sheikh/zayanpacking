31b4aaab50ad387715311d0bce874f93a0ba7fa8
Your Name <your-email@example.com>
2026-01-28 06:27:57 -0800

    fix line endings

--- frontend/zayanpack/src/pages/Home/Home.jsx
+++ frontend/zayanpack/src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react'
 import './Home.css'
 import { Context } from '../../context/Storecontext.jsx';
+import header_image from '../../assets/image copy.png'
 
 function Home() {
     const { check } = useContext(Context)
     return (
         <div className='home'>
             <div className="home-container">
-                <p>Home</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi atque quos explicabo officiis unde. Eveniet, amet provident atque, 
-                    maxime blanditiis aperiam .</p>
+               <img src={header_image} alt="" />
             </div>
         </div>
     )
